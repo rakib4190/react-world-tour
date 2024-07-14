d1a93282cdfc0f5a347c727f10b57f7d6baa0712
Rakibul Islam <mrislam4141@gmail.com>
2024-07-14 09:51:51 +0600

    rest countries json data loaded successfully

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Countries = () => {
+    const [countries, setCountries] =useState([]);
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    },[])
     return (
         <div>
             <h2>Countries :</h2>
